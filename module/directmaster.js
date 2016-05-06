8fe2382ba9093df711164257d36800e003405a3e
Selim Oruc <selim.oruc40@gmail.com>
2016-05-06 12:06:06 +0200

    adasd

---
+++ module/directmaster.js
@@ -0,0 +1,43 @@
+//import the file system functionality
+var fs = require ("fs")
+var foldertoread = process.argv[2]
+
+function masterf(){
+
+
+
+
+
+if (foldertoread == undefined)
+{
+    console.log ("please specify a folder")
+    return
+}
+
+fs.readdir (foldertoread, function (error,files){
+    if (error)
+    {
+        console.log ("error" + error)
+        throw error
+    } 
+    
+    
+   files.forEach(function (file)
+   
+   {
+      fs.readFile(foldertoread + "/" + file,"utf8", function (error, filecontents)
+      {
+          if (error)
+          {
+                     console.log ("error" + error)
+                     throw error
+          }
+          filecontents = JSON.parse (filecontents)
+          console.log (filecontents)
+      }
+     )
+   })
+})
+}
+
+module.exports.test = masterf
--- src/app.js
+++ src/app.js
@@ -1,36 +1,3 @@
-//import the file system functionality
-var fs = require ("fs")
-var foldertoread = process.argv[2]
+var dirnast = require ("../module/directmaster")
 
-
-
-if (foldertoread == undefined)
-{
-    console.log ("please specify a folder")
-    return
-}
-
-fs.readdir (foldertoread, function (error,files){
-    if (error)
-    {
-        console.log ("error" + error)
-        throw error
-    } 
-    
-    
-   files.forEach(function (file)
-   
-   {
-      fs.readFile(foldertoread + "/" + file,"utf8", function (error, filecontents)
-      {
-          if (error)
-          {
-                     console.log ("error" + error)
-                     throw error
-          }
-          filecontents = JSON.parse (filecontents)
-          console.log (filecontents)
-      }
-     )
-   })
-})
+dirnast.test()
